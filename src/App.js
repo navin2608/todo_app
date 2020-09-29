a76e6b9ae176709bad533ed762e190fccddfa4f5
your_name_here <msnsnavin@gmail.com>
2020-09-29 21:07:18 +0530

    We can simply add javascript within curly braces {}

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import React, { useState } from 'react';
 
 import './App.css';
 
+// We can simply add javascript within curly braces {}
+
 function App() {
-  const [todos, setTodos] = useState(['exercise','watch clever programmer {1+1}','eating snacks',"navin wants to livestream"]);
-  const [input, setInput] = useState('');
   return (
     <div className="App">
       <h1> hello navin </h1>
-      <input value={input} onChange={event=> setInput()}  />
-      <button> Add Todo </button>
       <ul>
-        {todos.map(todo =>(
-          <li>{todo}</li>
-        ))}
-        
+        <li>wake up{1 + 1}</li>
+        <li>watering the plants</li>
+
       </ul>
+
+
+
     </div>
-  );  
+  );
 }
 
 export default App;
