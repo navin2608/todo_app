23d0c93b0096e708cbaea78b7f236362a730d27f
your_name_here <msnsnavin@gmail.com>
2020-10-01 10:56:08 +0530

    added material UI created the form because by hitting enter we can add the content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import { Button } from '@material-ui/core';
 import './App.css';
 
 // We can simply add javascript within curly braces {}
@@ -8,20 +8,31 @@ function App() {
   const [todos, setTodos] = useState(['wake up{1 + 1}', 'watering the plants'])
   const [input, setInput] = useState('')
   console.log(input);
+  const addTodo = (event) => {
+    //this will fire off when we click the button
+    event.preventDefault();     // will stop the REFRESH
+    console.log('i am working');
+    setTodos([...todos, input]);
+    setInput('');   //clear up the contents in the text field
+  }
 
   return (
     <div className="App">
       <h1> hello navin </h1>
-      <input value={input} onChange={event => setInput(event.target.value)} />
+      <form>
+        <input value={input} onChange={event => setInput(event.target.value)} />
+        <Button disabled={!input} variant="contained" color="primary" type="submit" onClick={addTodo}>
+          Primary
+        </Button>
+        {/* <button type="submit" onClick={addTodo}>Add Todo</button> */}
+        <ul>
+          {todos.map(todo => (
+            <li>{todo}</li>
+          ))
+          }
 
-      <button>Add Todo</button>
-      <ul>
-        {todos.map(todo => (
-          <li>{todo}</li>
-        ))
-        }
-
-      </ul>
+        </ul>
+      </form>
 
 
 
