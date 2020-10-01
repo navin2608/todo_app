2085da6e1474246887614a4a699fce20d5814161
your_name_here <msnsnavin@gmail.com>
2020-10-01 18:34:09 +0530

    created firebsae.js and added the config from firebase

--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,10 @@
 import { ListItemText, List, ListItem } from '@material-ui/core'
 import React from 'react'
-
+import './Todo.css'
 
 function Todo(props) {
     return (
-        <List>
+        <List className="todo_list">
             <ListItem>
                 <ListItemText primary="Todo..." secondary={props.text} />
             </ListItem>
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBxI2k7BFSX1y8yeAyRMexQEoVYWmpBni8",
+    authDomain: "todo-app-nvk.firebaseapp.com",
+    databaseURL: "https://todo-app-nvk.firebaseio.com",
+    projectId: "todo-app-nvk",
+    storageBucket: "todo-app-nvk.appspot.com",
+    messagingSenderId: "49337496003",
+    appId: "1:49337496003:web:e11fe45e280ab63e838d76",
+    measurementId: "G-SCXWWW2HNZ"
+};
