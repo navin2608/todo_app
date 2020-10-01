a92c9fa7b7496ff8e3fe2228a7850954f4194ac3
your_name_here <msnsnavin@gmail.com>
2020-10-01 09:46:33 +0530

    importing the state() import React, { useState } from 'react'; & using that to get the input <input value={input} onChange={event => setInput(event.target.value)} />

--- src/App.js
+++ src/App.js
@@ -5,12 +5,21 @@ import './App.css';
 // We can simply add javascript within curly braces {}
 
 function App() {
+  const [todos, setTodos] = useState(['wake up{1 + 1}', 'watering the plants'])
+  const [input, setInput] = useState('')
+  console.log(input);
+
   return (
     <div className="App">
       <h1> hello navin </h1>
+      <input value={input} onChange={event => setInput(event.target.value)} />
+
+      <button>Add Todo</button>
       <ul>
-        <li>wake up{1 + 1}</li>
-        <li>watering the plants</li>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))
+        }
 
       </ul>
 
